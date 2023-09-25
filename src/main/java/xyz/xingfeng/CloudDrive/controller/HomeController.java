c24a7ff20b3edc0cd92599c611682ab407973237
DTXingFeng <92532952+DTXingFeng@users.noreply.github.com>
2023-09-25 20:29:49 +0800

    1.0

--- src/main/java/xyz/xingfeng/CloudDrive/controller/HomeController.java
+++ src/main/java/xyz/xingfeng/CloudDrive/controller/HomeController.java
@@ -1,14 +1,18 @@
 package xyz.xingfeng.CloudDrive.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "index";
+    @GetMapping("/home/{id}")
+    public String home(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        return "index"; // 返回Thymeleaf模板
     }
 }
+
 
